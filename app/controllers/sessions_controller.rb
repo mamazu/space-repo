d13cbaf3b323ee934d73f2fb656c5a664575d6f0
Drees Dreessen <drees.dreessen@brille24.de>
2019-12-23 16:33:09 +0100

    Added Login Modal for failed Logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   # Render the login form
   def login
+    @error = request.GET[:error]
   end
 
   # Fill the session with the authenticated user
@@ -10,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      redirect_to '/login?error=true'
     end
   end
 
