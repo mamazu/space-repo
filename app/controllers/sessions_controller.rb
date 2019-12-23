861421e7192b6bd5c907c47abef9094eeb3d7edf
mamazu <14860264+mamazu@users.noreply.github.com>
2019-12-23 13:44:11 +0100

    Adding a login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password]) 
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def login
+  end
+
+  def logout
+    session[:user_id] = nil
+  end
+end
