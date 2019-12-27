b128e7cbdb20412436713e9ea3e3283a5be64e75
Drees Dreessen <drees.dreessen@brille24.de>
2019-12-27 14:36:37 +0100

    Styling of the Login Modal

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#login'
   post 'login', to: 'sessions#create'
-  get '/logout', to: 'sessions#logout'
+  get 'logout', to: 'sessions#logout'
   get '/', to: 'home_page#index', as: 'welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
