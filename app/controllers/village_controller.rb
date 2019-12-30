b39c0767f3473235444567437d4da3b75ec7e897
mamazu <14860264+mamazu@users.noreply.github.com>
2019-12-30 10:28:46 +0100

    Codestyle

--- app/controllers/village_controller.rb
+++ app/controllers/village_controller.rb
@@ -1,10 +1,10 @@
 class VillageController < ApplicationController
   def overview
     @user = current_user
-    if !Village.where(:name => 'Name').exists?
-      @village = Village.create(:name => 'Name', :level => 0, :position_x => 0, :position_y => 0)
+    if !Village.where(:name => "Name").exists?
+      @village = Village.create(:name => "Name", :level => 0, :position_x => 0, :position_y => 0)
     else
-      @village = Village.find_by(:name => 'Name')
+      @village = Village.find_by(:name => "Name")
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'village', to: 'village#overview', as: 'village_overview'
+  get "village", to: "village#overview", as: "village_overview"
   resources :users, only: [:new, :create]
 
-  get 'login', to: 'sessions#login'
-  post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#logout'
-  get '/', to: 'home_page#index', as: 'welcome'
+  get "login", to: "sessions#login"
+  post "login", to: "sessions#create"
+  get "logout", to: "sessions#logout"
+  get "/", to: "home_page#index", as: "welcome"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
